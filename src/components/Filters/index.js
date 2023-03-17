29db5cdebad10c4dfa509a014a9bd193af38c47e
LCampi01 <lucas.ezequiel001@gmail.com>
2023-03-17 15:47:58 -0300

    fix(filters): clean fields when clean button is clicked

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -4,9 +4,15 @@ import CardContent from "@mui/material/CardContent";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
-
+import get from 'lodash/get';
 
 function Filters({filters, setFilters, search}) {
+
+  const cleanFilters = () => {
+    setFilters({name: '', username: '', email: ''});
+    search();
+  }
+
   return (
     <Card sx={{ minWidth: 275, m: "20px 0" }}>
       <CardContent sx={{ alignItems: "center" }}>
@@ -16,6 +22,7 @@ function Filters({filters, setFilters, search}) {
               id="outlined-basic"
               label="Name"
               variant="outlined"
+              value={get(filters, 'name', null)}
               onChange={(e) => setFilters({...filters, name: e.target.value})}
             />
           </Grid>
@@ -24,6 +31,7 @@ function Filters({filters, setFilters, search}) {
               id="outlined-basic"
               label="Username"
               variant="outlined"
+              value={get(filters, 'username', null)}
               onChange={(e) => setFilters({...filters, username: e.target.value})}  
             />
           </Grid>
@@ -32,11 +40,12 @@ function Filters({filters, setFilters, search}) {
               id="outlined-basic"
               label="email"
               variant="outlined"
+              value={get(filters, 'email', null)}
               onChange={(e) => setFilters({...filters, email: e.target.value})}
             />
           </Grid>
           <Grid item xs={2} sx={{margin:'auto'}}>
-            <Button fullWidth variant="contained" onClick={() => search()}>Clean filters</Button>
+            <Button fullWidth variant="contained" onClick={() => cleanFilters()}>Clean filters</Button>
           </Grid>
           <Grid item xs={2} sx={{margin:'auto'}}>
             <Button fullWidth variant="contained" onClick={() => search(filters)}>Search</Button>
